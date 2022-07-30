d6a28e04275e886e79206000def6f2426c0ce90f
sujit <sujitbokade2014@gmail.com>
2022-07-30 11:42:49 +0530

    [Sujit]Initialize Cards

---
+++ src/com/bridgelabz/deckofcard/DeckOfCards.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.deckofcard;
+
+public class DeckOfCards {
+    String[]  suits = new String[] { "Clubs", "Hearts", "Diamond", "Spades" };
+    String[] ranks = new String[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    String[][] deckOfCards = new String[4][13];
+
+    public void initialiseCards(){
+        for(int i=0;i<suits.length;i++){
+            for(int j=0;j<ranks.length;j++){
+                deckOfCards[i][j] = suits[i]+' '+ranks[j];
+            }
+        }
+    }
+
+    public void print(){
+        for(int i=0;i<suits.length;i++){
+            for(int j=0;j<ranks.length;j++){
+                System.out.println(deckOfCards[i][j]);
+            }
+        }
+    }
+}
---
+++ src/com/bridgelabz/deckofcard/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.deckofcard;
+
+public class Main {
+    public static void main(String[] args) {
+        DeckOfCards deckOfCards = new DeckOfCards();
+        deckOfCards.initialiseCards();
+        deckOfCards.print();
+
+    }
+}
